78c930bb5e0f3c1afdeff73b4af88d8dd7f725bd
ledleledle <leontodsky@gmail.com>
2020-04-07 20:07:34 +0700

    bersih2 lagi + script upload + update dikit

--- part/sidebar.php
+++ part/sidebar.php
@@ -18,9 +18,13 @@ foreach ($pecahjudul as $w) {
     <ul class="sidebar-menu">
       <li <?php echo ($page == "Dashboard") ? "class=active" : ""; ?>><a class="nav-link" href="index.php"><i class="fas fa-fire"></i><span>Dashboard</span></a></li>
       <li class="menu-header">Menu</li>
-      <li><a class="nav-link" href="404.php"><i class="fas fa-stethoscope"></i> <span>Rawat Jalan</span></a></li>
-      <li <?php echo ($page == "Data Pasien" || @$page1 == "det") ? "class=active" : ""; ?>><a class="nav-link" href="pasien.php"><i class="fas fa-user-injured"></i> <span>Data Pasien</span></a></li>
-      <!-- <li><a class="nav-link" href="blank.html"><i class="fas fa-skull"></i> <span>Foto Rotgen Pasien</span></a></li> -->
+      <li class="dropdown <?php echo ($page == "Data Pasien" || @$page1 == "det") ? "active" : ""; ?>">
+        <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-stethoscope"></i> <span>Pasien</span></a>
+        <ul class="dropdown-menu">
+          <li><a class="nav-link" href="404.php">Rawat Jalan</a></li>
+          <li <?php echo ($page == "Data Pasien") ? "class=active" : ""; ?>><a class="nav-link" href="pasien.php">Riwayat Pasien</a></li>
+        </ul>
+      </li>
       <li <?php echo ($page == "Data Pegawai") ? "class=active" : ""; ?>><a href="pegawai.php" class="nav-link"><i class="fas fa-users"></i> <span>Data Pegawai</span></a></li>
       <li class="dropdown <?php echo ($page1 == "ruang") ? "active" : ""; ?>">
         <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-bed"></i> <span>Rawat Inap</span></a>
